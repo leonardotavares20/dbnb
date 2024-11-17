b51eeacb1eb1318997fa23f405e56e05fd8f8ead
leonardotavares20 <leo.tav.12345@gmail.com>
2024-11-17 03:12:07 -0300

    feat: components - add nav component

---
+++ resources/js/components/header/nav.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Input from "../inputs";
+    import Button from "../button/button.svelte";
+</script>
+
+<nav
+    class="flex rounded-[20px] fixed top-0 w-full items-center min-h-32 pt-5 justify-between px-10"
+>
+    <a class="text-3xl" href="/">DBNB</a>
+    <Input.text placeholder="SEARCH MY NEXT CAR" />
+    <div>
+        <Button>LOGIN</Button>
+        <Button>REGISTER</Button>
+    </div>
+</nav>
+
+<style>
+</style>
