52dfb73cb756e3c57eaa9c583cfd6131026a7c05
leonardotavares20 <leo.tav.12345@gmail.com>
2024-11-17 03:17:33 -0300

    chore: header - text uppercase

--- resources/js/components/header/header.svelte
+++ resources/js/components/header/header.svelte
@@ -2,6 +2,6 @@
     import Nav from "./nav.svelte";
 </script>
 
-<header class="h-full w-full -top-[85%] shadow-md shadow-slate-200 absolute rounded-b-[20px]">
+<header class="h-full w-full -top-[85%] shadow-md shadow-slate-200 absolute rounded-b-[20px] uppercase">
     <Nav />
 </header>
