3328c6bfe71061e5985aa6a28ef635bfe849b4ee
leonardotavares20 <leo.tav.12345@gmail.com>
2024-11-19 03:52:05 -0300

    feat: button - add sm variant button

--- resources/js/components/button/button.svelte
+++ resources/js/components/button/button.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
     export let type: "button" | "submit" = "button";
+    export let sm: boolean = false;
 </script>
 
-<button {type}>
+<button class:sm class="rounded-full px-4 py-2 bg-black text-white w-full" {type}>
     <slot />
 </button>
+
+<style>
+    .sm {
+        @apply h-[55px] max-w-[150px] min-w-[130px] ;
+    }
+</style>
