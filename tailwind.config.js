d0983da01cbb9752e08798c0f514037f0a4f4160
leonardotavares20 <leo.tav.12345@gmail.com>
2024-11-23 02:40:27 -0300

    chore: config extra path for tailwind blade content comp

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        './**/*.blade.php',
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/**/*.blade.php",
