646f96c360ad16835c191e9936be66ba95e6c392
leonardotavares20 <leo.tav.12345@gmail.com>
2024-11-23 02:39:48 -0300

    feat: global - add global layout for blade views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Página Inicial')
+
+@section('content')
+  <h1 class="text-3xl">About page</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Krona+One&display=swap" rel="stylesheet">
+    @vite('resources/js/app.js')
+    <title>{{ $title ?? 'Título Padrão' }}</title>
+  </head>
+  <body class="font-krona h-full">
+      @yield('content')
+  </body>
+</html>
